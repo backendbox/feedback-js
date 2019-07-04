169d362005fb09f9714f835d473d6fe246fde609
backendbox <contact@backendbox.net>
2019-07-04 16:17:44 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,25 @@
 var btn = document.createElement("BUTTON");
-btn.innerHTML = "CLICK ME";
+btn.innerHTML = `<a href="#" class="float">
+<i class="fa fa-plus my-float"></i>
+</a>`;
 document.body.appendChild(btn);
+var style = document.createElement('style');
+  style.innerHTML = `
+.float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#0C9;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+	box-shadow: 2px 2px 3px #999;
+}
+
+.my-float{
+	margin-top:22px;
+}
+  `;
+  document.head.appendChild(style);
